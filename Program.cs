5c2bfe952a286b65be41b101afba917437404181
yanislav94 <yanislav.tomov94@gmail.com>
2023-01-05 15:54:25 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making API Call...");
+            using var client = new HttpClient();
+
+            //napravi si date pole
+            //parse html
+            HttpResponseMessage response = client.GetAsync("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=2006-01-02").Result;
+            
+            if(response.IsSuccessStatusCode) {
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+
+            }
+
+        }
+    }
+}
