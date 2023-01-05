6572f7ed4238dae36d89ca62468eaee9a4acfa0d
yanislav94 <yanislav.tomov94@gmail.com>
2023-01-05 16:35:36 +0200

    got all regions in a list

--- Program.cs
+++ Program.cs
@@ -11,16 +11,34 @@ namespace Test
             Console.WriteLine("Making API Call...");
             using var client = new HttpClient();
 
-            //napravi si date pole
-            //parse html
-            HttpResponseMessage response = client.GetAsync("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=2006-01-02").Result;
-            
-            if(response.IsSuccessStatusCode) {
-            string result = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine(result);
+            DateOnly date = new DateOnly(2006, 01, 02);
+            Console.WriteLine(date);
+        
+            HttpResponseMessage response = client.GetAsync("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=" + date).Result;
 
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                List<string> regions = getAllRegions(result);
+                regions.ForEach(Console.WriteLine);
             }
 
+        }
+
+        static List<string> getAllRegions(string result)
+        {
+            List<string> regions = new List<string>();
+            string[] subStrings = result.Split("<span class=\"style6\">");
+
+            for (int i = 1; i < subStrings.Length; i++)
+            {
+                string input = subStrings[i];
+                int index = input.IndexOf("<");
+                input = input.Substring(0, index);
+
+                regions.Add(input);
+            }
+            return regions;
         }
     }
 }
