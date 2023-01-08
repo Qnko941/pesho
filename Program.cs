844ecbc9ef5528e396bc8428993b1cd63df37a9e
yanislav94 <yanislav.tomov94@gmail.com>
2023-01-08 11:26:52 +0200

    Moved downloading html to a stand alone service

--- Program.cs
+++ Program.cs
@@ -8,48 +8,22 @@ namespace RealEstateApp
     class Program
     {
 
-        private WeeklyReportDto GetData(DateOnly date)
-        {
-            IServiceProvider serviceProvider = null;
-            serviceProvider.Register<ICacheProvider, CacheProvider>();
-            serviceProvider.Register<IMirelaDownloader, MirelaDownloader>();
+        // private WeeklyReportDto GetData(DateOnly date)
+        // {
+        //     var dataFileName = date.ToString("yyyy-MM-dd") + ".json";
+        //     if (File.Exists(dataFileName))
+        //     {
+        //         var content = File.ReadAllText(dataFileName);
+        //         return JsonSerializer.Deserialize<WeeklyReportDto>(content);
+        //     }
 
+        //     // var html = GetDataHtml(date);
+        //     // var result = Parse(html);
 
-
-
-
-
-            var dataFileName = date.ToString("yyyy-MM-dd") + ".json";
-            if (File.Exists(dataFileName))
-            {
-                var content = File.ReadAllText(dataFileName);
-                return JsonSerializer.Deserialize<WeeklyReportDto>(content);
-            }
-
-            var html = GetDataHtml(date);
-            var result = Parse(html);
-
-            var jsonContent = JsonSerializer.Deserialize(result);
-            File.WriteAllText(dataFileName, jsonContent);
-            return result;
-        }
-
-        static string GetDataHtml(DateOnly date)
-        {
-            var dataFileName = date.ToString("yyyy-MM-dd") + ".html";
-            if (File.Exists(dataFileName))
-            {
-                return File.ReadAllText(dataFileName);
-            }
-
-            using var client = new HttpClient();
-            using var response = client.GetAsync("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=" + date).Result;
-            response.EnsureSuccessStatusCode();
-
-            var content = response.Content.ReadAsStringAsync().Result;
-            File.WriteAllText(dataFileName, content);
-            return content;
-        }
+        //     // var jsonContent = JsonSerializer.Deserialize(result);
+        //     // File.WriteAllText(dataFileName, jsonContent);
+        //     return result;
+        // }
 
         private WeeklyReportDto Parse(string html)
         {
@@ -69,10 +43,9 @@ namespace RealEstateApp
 
             DateOnly date = new DateOnly(2006, 01, 02);
             Console.WriteLine(date);
-
+            MirelaDownloader mirelaDownloader = new MirelaDownloader();
             HttpResponseMessage response = client.GetAsync("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=" + date).Result;
-            GetDataHtml(date);
-
+            mirelaDownloader.GetDataHtml(date);
 
         }
 
