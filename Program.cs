6ca50d5882d20a56fa6c2f17defbc1b2f21999ea
yanislav94 <yanislav.tomov94@gmail.com>
2023-01-11 10:13:13 +0200

    Playing with nodes

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
+using HtmlAgilityPack;
 
 namespace RealEstateApp
 {
     class Program
     {
 
-        // private WeeklyReportDto GetData(DateOnly date)
+        // public WeeklyReportDto GetData(DateOnly date)
         // {
+        //     MirelaDownloader mirelaDownloader = new MirelaDownloader();
         //     var dataFileName = date.ToString("yyyy-MM-dd") + ".json";
         //     if (File.Exists(dataFileName))
         //     {
@@ -17,18 +19,13 @@ namespace RealEstateApp
         //         return JsonSerializer.Deserialize<WeeklyReportDto>(content);
         //     }
 
-        //     // var html = GetDataHtml(date);
-        //     // var result = Parse(html);
+        //     var html = mirelaDownloader.GetDataHtml(date);
+        //     var result = Parse(html);
 
-        //     // var jsonContent = JsonSerializer.Deserialize(result);
-        //     // File.WriteAllText(dataFileName, jsonContent);
+        //     var jsonContent = JsonSerializer.Deserialize<WeeklyReportDto>(result);
+        //     File.WriteAllText(dataFileName, jsonContent);
         //     return result;
         // }
-
-        private WeeklyReportDto Parse(string html)
-        {
-            return null;
-        }
 
         //# Setup DI
         //#Iterate all dates
@@ -44,25 +41,51 @@ namespace RealEstateApp
             DateOnly date = new DateOnly(2006, 01, 02);
             Console.WriteLine(date);
             MirelaDownloader mirelaDownloader = new MirelaDownloader();
+
+
             HttpResponseMessage response = client.GetAsync("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=" + date).Result;
-            mirelaDownloader.GetDataHtml(date);
+            var a = HtmlAgilityPackParse(mirelaDownloader.GetDataHtml(date));
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
 
         }
 
-        static List<string> getAllRegions(string result)
+        public static IEnumerable<string> HtmlAgilityPackParse(string html)
         {
-            List<string> regions = new List<string>();
-            string[] subStrings = result.Split("<span class=\"style6\">");
+            HtmlDocument htmlSnippet = new HtmlDocument();
+            htmlSnippet.LoadHtml(html);
 
-            for (int i = 1; i < subStrings.Length; i++)
+            List<string> tds = new List<string>();
+
+            foreach (HtmlNode link in htmlSnippet.DocumentNode.SelectNodes("//tr").Skip(3))
             {
-                string input = subStrings[i];
-                int index = input.IndexOf("<");
-                input = input.Substring(0, index);
 
-                regions.Add(input);
+                var a = link.InnerText.Split("&euro;&nbsp;");
+                foreach (var item in a)
+                {
+                    Console.WriteLine(item);
+                }
+                // var b = link.SelectNodes("//td");
+
+                // foreach (var item in b)
+                // {
+                //     Console.WriteLine(item.InnerText);
+                // }
+                // var arr = link.InnerText.Split("euro;");
+
+                // if (arr.Length > 2)
+                // {
+                //     foreach (var item in arr)
+                //     {
+                //         Console.WriteLine(item);
+                //     }
+                // }
+                //tds.Add(att.Value);
             }
-            return regions;
+            return tds;
         }
     }
 }
