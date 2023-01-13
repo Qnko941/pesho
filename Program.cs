16bdb0ea5c1834c9af83bf77b9f824a793883321
yanislav94 <yanislav.tomov94@gmail.com>
2023-01-13 19:13:21 +0200

    Parsed html file to JSON objects

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
+﻿using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using HtmlAgilityPack;
 
 namespace RealEstateApp
@@ -59,33 +58,59 @@ namespace RealEstateApp
             htmlSnippet.LoadHtml(html);
 
             List<string> tds = new List<string>();
-
-            foreach (HtmlNode link in htmlSnippet.DocumentNode.SelectNodes("//tr").Skip(3))
-            {
-
-                var a = link.InnerText.Split("&euro;&nbsp;");
-                foreach (var item in a)
-                {
-                    Console.WriteLine(item);
-                }
-                // var b = link.SelectNodes("//td");
-
-                // foreach (var item in b)
-                // {
-                //     Console.WriteLine(item.InnerText);
-                // }
-                // var arr = link.InnerText.Split("euro;");
-
-                // if (arr.Length > 2)
-                // {
-                //     foreach (var item in arr)
-                //     {
-                //         Console.WriteLine(item);
-                //     }
-                // }
-                //tds.Add(att.Value);
-            }
+            var rows = htmlSnippet.DocumentNode.SelectNodes("//tr").Skip(3).Select(ParsePricesRow);
+            var options = new JsonSerializerOptions { 
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true };
+            var json = JsonSerializer.Serialize(rows, options);
+            tds.Add(json);
             return tds;
         }
+
+        private static Price ParsePrice(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text) || text == "-")
+            {
+                return null;
+            }
+            var parsedString = text.Replace("&nbsp;", " ");
+            //&euro; 22 900&euro; 477/м2,
+            var priceParts = parsedString.Split("&euro; ");
+            var total = double.Parse(priceParts[1].Replace(" ", ""));
+            var psm = double.Parse(priceParts[2].Replace(" ", "").Replace("/м2", ""));
+            //parsedString = parsedString.Replace("&euro;", "");
+
+
+            return new Price
+            {
+                Total = total,
+                Psm = psm
+            };
+        }
+        private static PricesRow ParsePricesRow(HtmlNode row)
+        {
+            var cells = row.ChildNodes;
+            System.Console.WriteLine(cells[0].InnerText);
+            return new PricesRow
+            {
+                Region = cells[0].InnerText,
+                Room1 = ParsePrice(cells[1].InnerText),
+                Room2 = ParsePrice(cells[2].InnerText),
+                Room3 = ParsePrice(cells[3].InnerText),
+            };
+        }
+    }
+
+    class Price
+    {
+        public double Total { get; set; }
+        public double Psm { get; set; }
+    }
+    class PricesRow
+    {
+        public string Region { get; set; }
+        public Price Room1 { get; set; }
+        public Price Room2 { get; set; }
+        public Price Room3 { get; set; }
     }
 }
